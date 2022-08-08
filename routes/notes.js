731fedd088ed2237fe654e8a307f0216fa004530
harismehboob142 <harismehboob2019@gmail.com>
2022-08-08 15:14:56 +0500

    backend completed API is created. Now we are going for frontend

--- routes/notes.js
+++ routes/notes.js
@@ -26,14 +26,20 @@ router.post('/addNotes', fetchuser,
     async (req, res) => {
 
         const { title, description, tag } = req.body;
-        // checking if any error occurs in validation
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
+        try {
+            // checking if any error occurs in validation
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+            const note = new Notes({ title, description, tag, user: req.user.id }) //note is local and Notes is for new object of schema. to store the data according to schema in database
+            const savedNote = await note.save();
+            res.json(savedNote);
         }
-        const note = new Notes({ title, description, tag, user: req.user.id }) //note is local and Notes is for new object of schema. to store the data according to schema in database
-        const savedNote = await note.save();
-        res.json(savedNote);
+        catch (error) {
+            console.error(error.message);
+            res.status(400).send({ error: "Error in fetchNotes Route" })
+        }
 
     })
 
@@ -42,37 +48,44 @@ router.put('/updateNotes/:id', fetchuser,
     async (req, res) => {
         const { title, description, tag } = req.body;
         const newNote = {};
-        if (title) { newNote.title = title };
-        if (description) { newNote.description = description };
-        if (tag) { newNote.tag = tag };
-        // finding the note to be updated, that if it is present in db
-        let note = await Notes.findById(req.params.id);
-        if (!note) { return res.status(404).send("Note not found") }
-        // check if the note user and db user are same (user who possesses the node is trying to update it)
-        if (note.user.toString() !== req.user.id) {
-            return res.status(401).send("Not allowed");
+        try {
+            if (title) { newNote.title = title };
+            if (description) { newNote.description = description };
+            if (tag) { newNote.tag = tag };
+            // finding the note to be updated, that if it is present in db
+            let note = await Notes.findById(req.params.id);
+            if (!note) { return res.status(404).send("Note not found") }
+            // check if the note user and db user are same (user who possesses the node is trying to update it)
+            if (note.user.toString() !== req.user.id) {
+                return res.status(401).send("Not allowed");
+            }
+            note = await Notes.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
+            res.json(note);
         }
-        note = await Notes.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
-        res.json(note);
+        catch (error) {
+            console.error(error.message);
+            res.status(400).send({ error: "Error in fetchNotes Route" })
+        }
     })
 // ROUTE 4 Deleting notes : DELETE "/api/notes/deleteNotes" requires Login
 
-router.put('/deleteNotes/:id', fetchuser,
+router.delete('/deleteNotes/:id', fetchuser,
     async (req, res) => {
-        const { title, description, tag } = req.body;
-        const newNote = {};
-        if (title) { newNote.title = title };
-        if (description) { newNote.description = description };
-        if (tag) { newNote.tag = tag };
-        // finding the note to be updated, that if it is present in db
-        let note = await Notes.findById(req.params.id);
-        if (!note) { return res.status(404).send("Note not found") }
-        // check if the note user and db user are same (user who possesses the node is trying to update it)
-        if (note.user.toString() !== req.user.id) {
-            return res.status(401).send("Not allowed");
+        try {
+            // finding the note to be deleted, that if it is present in db
+            let note = await Notes.findById(req.params.id);
+            if (!note) { return res.status(404).send("Note not found") }
+            // check if the note user and db user are same (user who possesses the node is trying to update it)
+            if (note.user.toString() !== req.user.id) {
+                return res.status(401).send("Not allowed");
+            }
+            note = await Notes.findByIdAndDelete(req.params.id);
+            res.json({ "Success": "Note has been deleted", note: note });
+
+        } catch (error) {
+            console.error(error.message);
+            res.status(400).send({ error: "Error in fetchNotes Route" })
         }
-        note = await Notes.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
-        res.json(note);
     })
 
 
