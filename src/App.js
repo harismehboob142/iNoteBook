34cabf06504e63265c313f41fdde24b7f6219b65
harismehboob142 <harismehboob2019@gmail.com>
2022-08-24 22:35:50 +0500

    adding navbar, componenets and routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import Navbar from './components/Navbar';
+import { Home } from './components/Home';
+import { About } from './components/About';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      this is frontend
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<About />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
