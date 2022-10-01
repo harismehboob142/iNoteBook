a7e20a1d2c3812e215209aeac9547d3d457bbc91
harismehboob142 <harismehboob2019@gmail.com>
2022-10-01 15:06:41 +0500

    working on edit note functionality

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -7,21 +7,25 @@ import Noteitem from './Noteitem';
 
 const Notes = () => {
     const context = useContext(NoteContext);
-    const [note, setNote] = useState({ title: "", description: "", tag: "" })
     const { notes, getNotes } = context;
     useEffect(() => {
         getNotes();
     }, [])
 
+    const ref = useRef(null);   //to get a reference to modal for using that reference to open the modal
+    const [note, setNote] = useState({ id: "", etitle: "", edescription: "", etag: "" })
+
     const updateNote = (currentNote) => {
         ref.current.click();       //to click the button used to open the modal
-
+        setNote({ id: currentNote._id, etitle: currentNote.title, edescription: currentNote.description, etag: currentNote.tag })
+        console.log(currentNote.edescription)
 
     }
-    const ref = useRef(null);   //to get a reference to modal for using that reference to open the modal
 
     const handleOnClick = (event) => {
         event.preventDefault();
+        console.log("note updated")
+
     }
     const onChange = (event) => {
         setNote({ ...note, [event.target.name]: event.target.value })  //get data from note using spread operator. save it as a list in []:"" like title:"reports on generic medicine"
@@ -32,7 +36,7 @@ const Notes = () => {
             <AddNote />
 
             {/* modal for editing note */}
-            <button ref={ref} type="button" className="btn btn-primary d-none" data-bs-toggle="modal" data-bs-target="#exampleModal" >
+            <button ref={ref} type="button" className="btn btn-primary " data-bs-toggle="modal" data-bs-target="#exampleModal" onClick={updateNote}>
                 Edit button
             </button>
 
@@ -49,22 +53,22 @@ const Notes = () => {
                             <form className='my-3'>
                                 <div className="mb-3">
                                     <label htmlFor="title" className="form-label">Title</label>
-                                    <input type="text" className="form-control" id="etitle" name="etitle" aria-describedby="emailHelp" onChange={onChange} value={note.title} />
+                                    <input type="text" className="form-control" id="etitle" name="etitle" aria-describedby="emailHelp" onChange={onChange} value={note.etitle} />
                                 </div>
                                 <div className="mb-3">
                                     <label htmlFor="description" className="form-label">Description</label>
-                                    <input type="text" className="form-control" id="edescription" name="edescription" onChange={onChange} value={note.description} />
+                                    <input type="text" className="form-control" id="edescription" name="edescription" onChange={onChange} value={note.edescription} />
                                 </div>
                                 <div className="mb-3">
                                     <label htmlFor="tag" className="form-label">Tag</label>
-                                    <input type="text" className="form-control" id="etag" name="etag" onChange={onChange} value={note.tag} />
+                                    <input type="text" className="form-control" id="etag" name="etag" onChange={onChange} value={note.etag} />
                                 </div>
 
                             </form>
                         </div>
                         <div className="modal-footer">
                             <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" className="btn btn-primary">Update Note</button>
+                            <button type="button" onClick={handleOnClick} className="btn btn-primary">Update Note</button>
                         </div>
                     </div>
                 </div>
